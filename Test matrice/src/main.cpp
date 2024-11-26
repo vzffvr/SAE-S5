0e90c4606334414bdbac63e39e7aec101646408e
vzffvr <touraaugus@gmail.com>
2024-11-26 14:15:46 +0100

    Cablage ok PB de selection de ligne
    Si seulement 1 chiffre c est seulement que le 0 est effacé par la fonction serial. Si 2 alors col 0 ligne 2

--- Test matrice/src/main.cpp
+++ Test matrice/src/main.cpp
@@ -1,55 +1,87 @@
 #include <Arduino.h>
 
-uint8_t rows[2] = {11, 12};
-uint8_t cols[2] = {9, 10};
-int col_scan;
+uint8_t rows[3] = {12,11, 10};
+uint8_t cols[3] = {6,7,8};
+int cols_scan = 0;
+int cpt = 0;
+uint8_t btn = 00;
+uint8_t last_btn = 00;
+uint8_t btn_tab[25] = {00,01,02,10,11,12,20,21,22};
 
-void toucherBouton(uint8_t i, uint8_t j);
+void toucherBouton(uint8_t num_ligne, uint8_t num_col);
+void scan();
 
 void setup()
 {
   Serial.begin(115200);
-  for (int i = 0; i <= 1; i++)
+  for (int i = 0; i < 3; i++)
   {
-
     pinMode(rows[i], OUTPUT);
-    pinMode(cols[i], INPUT);
-    digitalWrite(cols[i], HIGH);
+    pinMode(cols[i], INPUT_PULLUP);
   }
+
 }
 
 void loop()
 {
-  // Regarde si un bouton est enfoncé
-  for (int i = 0; i <= 1; i++)
+  for (int num_ligne = 0; num_ligne < 3; num_ligne++)
   {
-    digitalWrite(rows[0], HIGH);
-    digitalWrite(rows[1], HIGH);
-    //digitalWrite(rows[2], HIGH);
+    digitalWrite(rows[0], LOW);
+    digitalWrite(rows[1], LOW);
+    digitalWrite(rows[2], LOW);
+    digitalWrite(rows[num_ligne], HIGH);
 
-    digitalWrite(rows[i], LOW);
-    for (int j = 0; j <= 1; j++)
+    for (int num_col = 0; num_col < 3; num_col++)
     {
-      col_scan = digitalRead(cols[j]);
-      if (col_scan == LOW)
+      cols_scan = digitalRead(cols[num_col]);
+      if (cols_scan == LOW)
       {
-        // Lorsqu'un bouton est enfoncé, appel de la fonction toucherBouton
-        // pour savoir quel bouton est enfoncé
-        toucherBouton(i, j);
-        delay(300);
-      }
+      toucherBouton(num_col, num_ligne);    
+      delay(300);
+      }  
     }
+  }  
+  if(btn==last_btn)
+  {
+    cpt++;
   }
+  else
+  {
+    cpt=0;
+  }
+  if(cpt >= 10)
+  {
+    Serial.println(btn);
+    Serial.print("cpt = ");
+    Serial.println(cpt);
+    // Serial.print(num_col);
+    // Serial.println(num_ligne);
+  }
+  last_btn = btn;
 }
 
-void toucherBouton(uint8_t i, uint8_t j)
+  
+
+void toucherBouton(uint8_t num_col, uint8_t num_ligne)
 {
-  if (i == 0 && j == 0) // Bouton S1 enfoncé
-    Serial.println("Bas gauche");
-  if (i == 0 && j == 1) // Bouton S2 enfoncé
-    Serial.println("Haut gauche");
-  if (i == 1 && j == 0) // Bouton S3 enfoncé
-    Serial.println("Bas droit");
-  if (i == 1 && j == 1) // Bouton S4 enfoncé
-    Serial.println("Haut Droit");
+  if (num_col == 0 && num_ligne == 0) // Bouton S1 enfoncé
+    btn = btn_tab[0];
+  if (num_col == 0 && num_ligne == 1) // Bouton S2 enfoncé
+    btn = btn_tab[1];
+  if (num_col == 0 && num_ligne == 2) // Bouton S2 enfoncé
+    btn = btn_tab[2];
+
+  if (num_col == 1 && num_ligne == 0) // Bouton S3 enfoncé
+    btn = btn_tab[3];
+  if (num_col == 1 && num_ligne == 1) // Bouton S4 enfoncé
+    btn = btn_tab[4];
+  if (num_col == 1 && num_ligne == 2) // Bouton S4 enfoncé
+    btn = btn_tab[5];
+
+  if (num_col == 2 && num_ligne == 0) // Bouton S4 enfoncé
+    btn = btn_tab[6];   
+  if (num_col == 2 && num_ligne == 1) // Bouton S4 enfoncé
+    btn = btn_tab[7];
+  if (num_col == 2 && num_ligne == 2) // Bouton S4 enfoncé
+    btn = btn_tab[8]; 
 }
