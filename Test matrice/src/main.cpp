40309bd71a43ea102529f6ccc274113b5ca6b8b1
vzffvr <touraaugus@gmail.com>
2024-11-28 19:55:33 +0100

    Marche pas 28/11

--- Test matrice/src/main.cpp
+++ Test matrice/src/main.cpp
@@ -1,12 +1,24 @@
 #include <Arduino.h>
 
+#define PERIODE 1000
+#define PERIODE_SCAN 300
+
+
 uint8_t rows[3] = {12,11, 10};
-uint8_t cols[3] = {6,7,8};
-int cols_scan = 0;
+uint8_t cols[3] = {7,8,9};
+bool cols_scan = false;
 int cpt = 0;
 uint8_t btn = 00;
 uint8_t last_btn = 00;
-uint8_t btn_tab[25] = {00,01,02,10,11,12,20,21,22};
+uint8_t btn_tab[25] = {00,01,02,10,11,12,20,21,22,99};
+
+uint8_t col_select = 0;
+uint8_t row_select = 0;
+
+uint32_t maintenant_debug  = 0;
+uint32_t maintenant_scan  = 0;
+
+
 
 void toucherBouton(uint8_t num_ligne, uint8_t num_col);
 void scan();
@@ -17,47 +29,134 @@ void setup()
   for (int i = 0; i < 3; i++)
   {
     pinMode(rows[i], OUTPUT);
-    pinMode(cols[i], INPUT_PULLUP);
+    pinMode(cols[i], INPUT);
   }
-
+  maintenant_debug = millis();
+  maintenant_scan = millis();
 }
 
 void loop()
 {
-  for (int num_ligne = 0; num_ligne < 3; num_ligne++)
+  if ( (maintenant_debug + PERIODE) < millis())
   {
-    digitalWrite(rows[0], LOW);
+    digitalWrite(rows[0], HIGH);
     digitalWrite(rows[1], LOW);
     digitalWrite(rows[2], LOW);
-    digitalWrite(rows[num_ligne], HIGH);
-
-    for (int num_col = 0; num_col < 3; num_col++)
+    if (digitalRead(cols[0]) == LOW)
     {
-      cols_scan = digitalRead(cols[num_col]);
-      if (cols_scan == LOW)
-      {
-      toucherBouton(num_col, num_ligne);    
-      delay(300);
-      }  
+      col_select = 0;
+      row_select = 0;
+    }  
+    if (digitalRead(cols[1]) == LOW)
+    {
+      col_select = 1;
+      row_select = 0;
+    }  
+    if (digitalRead(cols[2]) == LOW)
+    {
+      col_select = 2;
+      row_select = 0;
     }
-  }  
-  if(btn==last_btn)
-  {
-    cpt++;
+
+    digitalWrite(rows[0], LOW);
+    digitalWrite(rows[1], HIGH);
+    digitalWrite(rows[2], LOW);
+    if (digitalRead(cols[0]) == LOW)
+    {
+      col_select = 0;
+      row_select = 1;
+    }  
+    if (digitalRead(cols[1]) == LOW)
+    {
+      col_select = 1;
+      row_select = 1;
+    }  
+    if (digitalRead(cols[2]) == LOW)
+    {
+      col_select = 2;
+      row_select = 1;
+    }
+
+    digitalWrite(rows[0], LOW);
+    digitalWrite(rows[1], LOW);
+    digitalWrite(rows[2], HIGH);
+    if (digitalRead(cols[0]) == LOW)
+    {
+      col_select = 0;
+      row_select = 2;
+    }  
+    if (digitalRead(cols[1]) == LOW)
+    {
+      col_select = 1;
+      row_select = 2;
+    }  
+    if (digitalRead(cols[2]) == LOW)
+    {
+      col_select = 2;
+      row_select = 2;
+    }
+
+    toucherBouton(col_select, row_select);
   }
-  else
+  
+
+    
+
+
+
+
+  // for (int num_ligne = 0; num_ligne < 3; num_ligne++)
+  // {
+  //   digitalWrite(rows[0], LOW);
+  //   digitalWrite(rows[1], LOW);
+  //   digitalWrite(rows[2], LOW);
+  //   digitalWrite(rows[num_ligne], HIGH);
+
+  //   for (int num_col = 0; num_col < 3; num_col++)
+  //   {
+  //     cols_scan = digitalRead(cols[num_col]);
+  //     if (cols_scan == LOW)
+  //     {
+
+  //       col_select = num_col;
+  //       row_select = num_ligne;
+
+  //     }  
+  //   }
+  // }  
+
+  
+
+  if( (maintenant_debug + PERIODE) < millis())
   {
-    cpt=0;
+    maintenant_debug = millis();
+    Serial.print(col_select);
+    Serial.print("   ");
+    Serial.println(row_select);
   }
-  if(cpt >= 10)
-  {
-    Serial.println(btn);
-    Serial.print("cpt = ");
-    Serial.println(cpt);
-    // Serial.print(num_col);
-    // Serial.println(num_ligne);
-  }
+
+  
   last_btn = btn;
+
+
+
+  // if((btn==last_btn) && (btn =! 99))
+  // {
+  //   cpt++;
+  // }
+  // else
+  // {
+  //   cpt=0;
+  // }
+  // if(cpt >= 10)
+  // {
+  //   Serial.println(btn);
+  //   Serial.print("cpt = ");
+  //   Serial.println(cpt);
+  //   // Serial.print(num_col);
+  //   // Serial.println(num_ligne);
+  // }
+  // last_btn = btn;
 }
 
   
@@ -84,4 +183,6 @@ void toucherBouton(uint8_t num_col, uint8_t num_ligne)
     btn = btn_tab[7];
   if (num_col == 2 && num_ligne == 2) // Bouton S4 enfoncé
     btn = btn_tab[8]; 
+
+  
 }
